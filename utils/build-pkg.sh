56abf5bb549994d3c8a7cbc3771dd7119068887c
Bas Wegh <bas.wegh@kit.edu>
2016-12-20 11:15:27 +0100

    no need for specific ubuntu branch

--- utils/build-pkg.sh
+++ utils/build-pkg.sh
@@ -5,6 +5,7 @@ CURRENT_DIR=`pwd`
 cd `dirname $0`
 cd ..
 SOURCE_DIR=`pwd`
+VERSION=`git describe --exact-match --abbrev=0`
 source ./utils/prepare_system.sh
 
 # expecting to be in $GOPATH/src/github.com/indigo-dc/ttsc
@@ -17,14 +18,10 @@ rm -fr pkg-build/*
 mkdir -p pkg-build/amd64
 GOOS=linux GOARCH=amd64 go build -o build/amd64/ttsc ttsc.go
 
-VERSION="0.0.1"
 
 case $DISTRIBUTION in
-    ubuntu)
-        go-bin-deb generate -a amd64 --version ${VERSION} -w pkg-build/amd64/ -o ttsc-amd64.deb
-        ;;
     debian)
-        go-bin-deb generate -a amd64 --version ${VERSION} -w pkg-build/amd64/ -o ttsc-amd64.deb
+        go-bin-deb generate -a amd64 --version ${VERSION} -w pkg-build/amd64/ -o ttsc-${VERSION}-amd64.deb
         ;;
     centos)
         echo "not yet supported"
